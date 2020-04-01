9f3499dc88dddc462549abe2690b2d91142ec4e9
ammi3 <758217673@qq.com>
2020-04-01 20:56:58 +0800

    resume

---
+++ README.md
@@ -0,0 +1,50 @@
+## Java工程师简历
+
+### 介绍
+
+- **本项目源于**：*https://github.com/zhaoky/flqin*。向作者表示深深的敬意。
+
+### 线上预览
+
+> http://116.62.25.133:8080/ammi3/
+
+### 项目截图
+
+![](https://supers1.oss-cn-hangzhou.aliyuncs.com/20200401090248.png)
+
+![](https://supers1.oss-cn-hangzhou.aliyuncs.com/20200401090307.png)
+
+![](https://supers1.oss-cn-hangzhou.aliyuncs.com/20200401090323.png)
+
+![](https://supers1.oss-cn-hangzhou.aliyuncs.com/20200401090344.png)
+
+![](https://supers1.oss-cn-hangzhou.aliyuncs.com/20200401090359.png)
+
+### 本地预览
+
+1：确保本地已经安装了`node.js/npm`
+
+2：运行`npm install`，`npm run dev`然后在本地8080端口就能看到
+
+### 编译部署
+
+1：`npm run build`
+
+2：将生成的dist文件夹下的文件放在服务器的tomcat中
+
+## 项目支持
+
+[@korey/Mvvm](https://github.com/zhaoky/mvvm)
+
+> 一个简易的 MVVM 框架，目前实现了 data-binding 和 view-refresh 的功能，仍在不断优化更新，欢迎学习交流
+
+[@korey/Fullpage](https://github.com/zhaoky/fullpage)
+
+> 一个简易的 Fullpage 框架，目前实现了上拉下拉，滑轮滚动，锚点直达的全屏翻页功能，仍在不断优化更新，欢迎学习交流。
+
+### 关于我
+
+- [个人网站](https://aimezhao.online)
+- [知乎](https://www.zhihu.com/people/mus-72-94)
+- [github](https://github.com/ammi3)
+
---
+++ src/data.js
@@ -0,0 +1,388 @@
+import eJm from './assets/logo_jm.png';
+import eLt from './assets/logo_lt.png';
+import eZh from './assets/logo_zh.png';
+import dHome from './assets/d_home.svg';
+import dSkills from './assets/d_skills.svg';
+import dExp from './assets/d_exp.svg';
+import dWorks from './assets/d_works.svg';
+import dContact from './assets/d_contact.svg';
+import sGithub from './assets/s_github.svg';
+import sSf from './assets/s_sf.svg';
+import sBlog from './assets/s_blog.svg';
+import sZh from './assets/s_zh.svg';
+import sJj from './assets/s_Jj.svg';
+import wJm from './assets/w_jm.jpg';
+import wLs from './assets/w_ls.jpg';
+
+export default {
+  cn: {
+    header: {
+      langList: ['中', '英'],
+      titleList: [
+        {
+          svg: dHome,
+          title: '首页'
+        },
+        {
+          svg: dSkills,
+          title: '能力'
+        },
+        {
+          svg: dExp,
+          title: '经历'
+        },
+        {
+          svg: dWorks,
+          title: '作品集'
+        },
+        {
+          svg: dContact,
+          title: '联系我'
+        }
+      ]
+    },
+    footer: ['Released under the MIT License', 'All Rights Reserved ', 'Copyright © 2014-2019 Korey Zhao'],
+    overview: {
+      infoList: ['19岁', '本科', '上海', '上学'],
+      quote: 'While the world sleep, you dream!',
+      desList: ['我是ammi3，Java后端工程师', '758217673yj@gmail.com']
+    },
+    skill: {
+      outCircleList: [
+        {
+          bg: {
+            backgroundColor: 'rgba(121,100,102,0.8)'
+          },
+          name: 'Java'
+        },
+        {
+          bg: {
+            backgroundColor: 'rgba(49,65,82,0.8)'
+          },
+          name: 'Spring'
+        },
+        {
+          bg: {
+            backgroundColor: 'rgba(76,157,160,0.8)'
+          },
+          name: 'SpringMvc'
+        },
+        {
+          bg: {
+            backgroundColor: 'rgba(66,66,66,0.8)'
+          },
+          name: '小程序'
+        },
+        {
+          bg: {
+            backgroundColor: 'rgba(193,131,106,0.8)'
+          },
+          name: 'SpringBoot'
+        },
+        {
+          bg: {
+            backgroundColor: 'rgba(117,148,179,0.8)'
+          },
+          name: 'SpringCloud'
+        },
+        {
+          bg: {
+            backgroundColor: 'rgba(71,83,94,0.8)'
+          },
+          name: 'vue'
+        },
+        {
+          bg: {
+            backgroundColor: 'rgba(147,147,189,0.8)'
+          },
+          name: 'Shiro'
+        }
+      ],
+      innerCircleList: [
+        {
+          bg: {
+            backgroundColor: 'rgba(179,164,140,0.8)'
+          },
+          name: '算法'
+        },
+        {
+          bg: {
+            backgroundColor: 'rgba(171,209,220,0.8)'
+          },
+          name: 'BootStrap'
+        },
+        {
+          bg: {
+            backgroundColor: 'rgba(238,215,163,0.8)'
+          },
+          name: 'AdminLTE'
+        },
+        {
+          bg: {
+            backgroundColor: 'rgba(207,184,178,0.8)'
+          },
+          name: 'swift菜鸡'
+        }
+      ],
+      desList: [
+        '擅长SSM框架并能优化SQL',
+        '每天坚持刷leetCode和剑指Offer的算法题',
+        '自己主导开发过多模块开发的项目',
+        '自我驱动能力强，有着较强的学习能力'
+      ]
+    },
+    exp: {
+      expList: [
+        {
+          title: '晴猫公司（上海）',
+          time: '2018年4月~至今',
+          post: '实习生',
+          img: {
+            backgroundImage: ``
+          },
+          contentList: ['负责开发一些微信小程序', '在上海科技馆上课', '协助开发一系列的网络课程']
+        }
+      ]
+    },
+    works: {
+      worksList: [
+        {
+          title: '商城系统',
+          des: '基于Maven多模块开发的商城系统。',
+          url: 'https://github.com/ammi3/myshop'
+        },
+        {
+          title: '平时写的demo合集',
+          des: '包括SSM框架，Shiro框架，SpringBoot，SpringCloud，Vue......',
+          url: 'https://github.com/ammi3/demoCollection'
+        },
+        {
+          title: '算法总结经验',
+          des: '包括一些基础的算法知识和在牛客网，leetCode上的刷题经验',
+          url: 'https://github.com/ammi3/algorithm'
+        }
+      ],
+      viewMore: 'github上面查看更多'
+    },
+    contact: {
+      title: '联系我',
+      desList1: ['灵感', '代码', '梦想', '未来'],
+      desList2: ['注重效率', '喜欢尝试，期待新鲜事物', 'Java即兴趣，兴趣即未来', '行路有良友，便是捷径', '带上我吧，一起看到更大的世界'],
+      download: '下载简历',
+      fileList: [
+        {
+          title: 'HTML版',
+          url: 'www.baidu.com'
+        },
+        {
+          title: 'PDF版',
+          url: 'www.baidu.com'
+        }
+      ],
+      typeList: [
+        {
+          icon: sGithub,
+          url: 'https://github.com/ammi3'
+        },
+        {
+          icon: sZh,
+          url: 'https://www.zhihu.com/people/mus-72-94'
+        },
+        {
+          icon: sJj,
+          url: 'https://aimezhao.online'
+        }
+      ]
+    }
+  },
+  en: {
+    header: {
+      langList: ['CN', 'EN'],
+      titleList: [
+        {
+          svg: dHome,
+          title: 'home'
+        },
+        {
+          svg: dSkills,
+          title: 'skills'
+        },
+        {
+          svg: dExp,
+          title: 'exp'
+        },
+        {
+          svg: dWorks,
+          title: 'works'
+        },
+        {
+          svg: dContact,
+          title: 'contact'
+        }
+      ]
+    },
+    footer: ['Released under the MIT License', 'All Rights Reserved ', 'Copyright © 2014-2019 Korey Zhao'],
+    overview: {
+      infoList: ['19years', 'B.S.', 'Shanghai', ' Students'],
+      quote: 'While the world sleep, you dream!',
+      desList: ['I am ammi3, Java Developer', '75821767yj@gmail.com']
+    },
+    skill: {
+      outCircleList: [
+        {
+          bg: {
+            backgroundColor: 'rgba(121,100,102,0.8)'
+          },
+          name: 'Java'
+        },
+        {
+          bg: {
+            backgroundColor: 'rgba(49,65,82,0.8)'
+          },
+          name: 'Spring'
+        },
+        {
+          bg: {
+            backgroundColor: 'rgba(76,157,160,0.8)'
+          },
+          name: 'SpringMvc'
+        },
+        {
+          bg: {
+            backgroundColor: 'rgba(66,66,66,0.8)'
+          },
+          name: '小程序'
+        },
+        {
+          bg: {
+            backgroundColor: 'rgba(193,131,106,0.8)'
+          },
+          name: 'SpringBoot'
+        },
+        {
+          bg: {
+            backgroundColor: 'rgba(117,148,179,0.8)'
+          },
+          name: 'SpringCloud'
+        },
+        {
+          bg: {
+            backgroundColor: 'rgba(71,83,94,0.8)'
+          },
+          name: 'vue'
+        },
+        {
+          bg: {
+            backgroundColor: 'rgba(147,147,189,0.8)'
+          },
+          name: 'Shiro'
+        }
+      ],
+      innerCircleList: [
+        {
+          bg: {
+            backgroundColor: 'rgba(179,164,140,0.8)'
+          },
+          name: '算法'
+        },
+        {
+          bg: {
+            backgroundColor: 'rgba(171,209,220,0.8)'
+          },
+          name: 'BootStrap'
+        },
+        {
+          bg: {
+            backgroundColor: 'rgba(238,215,163,0.8)'
+          },
+          name: 'AdminLTE'
+        },
+        {
+          bg: {
+            backgroundColor: 'rgba(207,184,178,0.8)'
+          },
+          name: 'swift菜鸡'
+        }
+      ],
+      desList: [
+        'Good at SSM framework and able to optimize SQL',
+        'Every day I keep brushing the algorithm of leetCode and sword finger Offer',
+        'Projects that lead the development of too many modules',
+        'Self-driven, lead and help the team grow'
+      ]
+    },
+    exp: {
+      expList: [
+        {
+          title: 'qingMao(shanghai)',
+          time: '2018.04-now',
+          post: 'intern',
+          img: {
+            backgroundImage: ``
+          },
+          contentList: [
+            'Responsible for developing some WeChat applets',
+            'Class at Shanghai Science and Technology Museum',
+            'Assist in developing a series of online courses'
+          ]
+        }
+      ]
+    },
+    works: {
+      worksList: [
+        {
+          title: 'myshop',
+          des: 'Mall system based on Maven multi-module development。',
+          url: 'https://github.com/ammi3/myshop'
+        },
+        {
+          title: 'demoCollection',
+          des: 'Including SSM framework, Shiro framework, SpringBoot, SpringCloud, Vue...',
+          url: 'https://github.com/ammi3/demoCollection'
+        },
+        {
+          title: 'algorithm',
+          des: 'Includes some basic algorithm knowledge and experience in solving problems on Niu Ke, LeetCode',
+          url: 'https://github.com/ammi3/algorithm'
+        }
+      ],
+      viewMore: 'Go to github see more'
+    },
+    contact: {
+      title: 'Contact Me',
+      desList1: ['INSPIRATION', 'CODE', 'DREAM', 'FUTURE'],
+      desList2: [
+        'Focus on efficiency, preference for agile development',
+        'Like to try, look forward to something new',
+        'Front is interested, interest is the future',
+        'Line road friends, is a shortcut',
+        'With me, see the bigger world together'
+      ],
+      download: 'Download Resume',
+      fileList: [
+        {
+          title: 'HTML',
+          url: 'www.baidu.com'
+        },
+        {
+          title: 'PDF',
+          url: 'www.baidu.com'
+        }
+      ],
+      typeList: [
+        {
+          icon: sGithub,
+          url: 'https://github.com/ammi3'
+        },
+        {
+          icon: sZh,
+          url: 'https://www.zhihu.com/people/mus-72-94'
+        },
+        {
+          icon: sJj,
+          url: 'https://aimezhao.online'
+        }
+      ]
+    }
+  }
+};
